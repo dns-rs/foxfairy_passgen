8186219238274a0c8fd327a7492e8cd8e1f6dbd8
dns-rs <denis.glavcic@gmail.com>
2022-12-13 15:32:38 +0000

    vowels to numbers hash

--- foxfairy_passgen.py
+++ foxfairy_passgen.py
@@ -160,42 +160,13 @@ deserts = [
     "lepeny"
 ]
 
-print('(L)ong or (S)hort?')
-x = input().lower()
+while True:
+    print('(L)ong or (S)hort? or (Q)uit...')
+    x = input().lower()
 
-if (x == "l"):
-    start = random.choice(types_start)
-
-    rand_one = random.randint(0, 2)
-
-    if (rand_one == 0):
-        mid = random.choice(vegetables)
-    elif (rand_one == 1):
-        mid = random.choice(fruits)
-    else:
-        mid = random.choice(meats)
-
-    rand_one = random.randint(0, 1)
-
-    if (rand_one == 0):
-        end = random.choice(types_end)
-    else:
-        end = random.choice(deserts)
-
-    os.system('clear')
-    print(start + mid + end)
-    print()
-    print()
-    print()
-    print()
-
-
-elif (x == "s"):
-
-    rand_one = random.randint(0, 1)
-
-    if (rand_one == 0):
-        start = random.choice(types_start)  
+    if (x == "l"):
+        os.system('clear')
+        start = random.choice(types_start)
 
         rand_one = random.randint(0, 2)
 
@@ -206,33 +177,96 @@ elif (x == "s"):
         else:
             mid = random.choice(meats)
 
-        os.system('clear')
-        print(start + mid)
-        print()
-        print()
-        print()
-        print()
-    else:
-        rand_one = random.randint(0, 2)
+        rand_one = random.randint(0, 1)
 
-        if (rand_one == 0):
-            start = random.choice(vegetables)
-        elif (rand_one == 1):
-            start = random.choice(fruits)
-        else:
-            start = random.choice(meats)
-        
         if (rand_one == 0):
             end = random.choice(types_end)
         else:
             end = random.choice(deserts)
+        
+        merged = start + mid + end
+        
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        nrs = ['4', '3', '1', '0', '_']
 
+        count = len(vowels)
+        crypt = merged
+        for c in range(int(count)):
+            crypt = crypt.replace(vowels[c], nrs[c])
+                                       
+        print(merged)
+        print(crypt)
+        print()
+        print()
+        print()
+
+
+    elif (x == "s"):
         os.system('clear')
-        print(start + end)
-        print()
-        print()
-        print()
-        print()
+        rand_one = random.randint(0, 1)
 
-    
+        if (rand_one == 0):
+            start = random.choice(types_start)  
+
+            rand_one = random.randint(0, 2)
+
+            if (rand_one == 0):
+                mid = random.choice(vegetables)
+            elif (rand_one == 1):
+                mid = random.choice(fruits)
+            else:
+                mid = random.choice(meats)
+
+            
+            merged = start + mid
+
+            vowels = ['a', 'e', 'i', 'o', 'u']
+            nrs = ['4', '3', '1', '0', '_']
+
+            count = len(vowels)
+            crypt = merged
+            for c in range(int(count)):
+                crypt = crypt.replace(vowels[c], nrs[c])
+                                        
+            print(merged)
+            print(crypt)
+            print()
+            print()
+            print()
+            print()
+        else:
+            os.system('clear')
+            rand_one = random.randint(0, 2)
+
+            if (rand_one == 0):
+                start = random.choice(vegetables)
+            elif (rand_one == 1):
+                start = random.choice(fruits)
+            else:
+                start = random.choice(meats)
+            
+            if (rand_one == 0):
+                end = random.choice(types_end)
+            else:
+                end = random.choice(deserts)
+
+            vowels = ['a', 'e', 'i', 'o', 'u']
+            nrs = ['4', '3', '1', '0', '_']
+            merged = start + end
+            count = len(vowels)
+            crypt = merged
+            for c in range(int(count)):
+                print(c)
+                crypt = crypt.replace(vowels[c], nrs[c])
+                                        
+            print(merged)
+            print(crypt)            
+            print()
+            print()
+            print()
+            print()
+    elif (x == "q"):
+        break
+
+        
 
